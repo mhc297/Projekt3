f3c78aaee80b4aa31fee2b3868ceabb338edef46
Mathew Cullen <matcullen88@gmail.com>
2016-11-21 18:03:52 -0500

    services -> events built

---
+++ services/events.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+function searchEvents(req, res, next){
+  const searchTerm = placeholder.value;
+  let lat = placeholderforHTML5.value;
+  let long = placeholderforHTML5.value;
+  let key = gUk0LS4bt0e1hejKIqA9gTRDaEXEcTHJ;
+  fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${searchTerm}&latlong=${lat, long}0&radius=15&apikey=${key}`)
+    .then(r => r.json())
+    .then((data) => {
+      res.rows = data;
+      next();
+    })
+    .catch((error) => {
+      console.log("Error is ", error);
+      res.error = error;
+      next();
+    });
+};
+
+module.exports = { searchEvents };
