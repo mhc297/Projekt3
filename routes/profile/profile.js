58cda3085df03399851d3afe30b8fb695af74910
lee <leerwilson@gmail.com>
2016-11-22 13:30:39 -0500

    resolve conflicts

--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 const router = require('express').Router();
 const { authenticate } = require('../../lib/auth');
 const { getEvent, deleteEvent } = require('../../models/event_model');
 
 
 router.get('/', authenticate, getEvent, (req, res) => {
-  res.render('users/profile');
+ res.render('users/profile');
 });
 
-router.delete('/user/profile/:_id', deleteEvent, (req, res) => {
-=======
-const profileRouter = require('express').Router();
-const { authenticate } = require('../../lib/authentication');
-const { getLikes, removeLikes } = require('../../models/likes');
-
-
-profileRouter.get('/', authenticate, getLikes, (req, res) => {
-  res.render('users/profile');
-});
-
-profileRouter.delete('/user/profile/:_id', removeLikes, (req, res) => {
->>>>>>> 643992bb060af930260004b4eb8622265635e5c0
-  res.redirect('/user/profile');
+profileRouter.delete('/user/profile/:_id', deleteEvent, (req, res) => {
+ res.redirect('/user/profile');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,13 +7,9 @@ const express         = require('express');
 const logger          = require('morgan');
 const path            = require('path');
 const app             = express();
-<<<<<<< HEAD
-const apiRouter        = require('./routes/api/apiRoute.js');
-=======
->>>>>>> 643992bb060af930260004b4eb8622265635e5c0
 const authRouter      = require('./routes/auth/auth.js');
 const loginRouter     = require('./routes/login/login.js');
-const apiRoute        = require('./routes/api/apiRoute.js');
+const apiRouter        = require('./routes/api/apiRoute.js');
 const profileRouter   = require('./routes/profile/profile.js');
 const regRouter       = require('./routes/register/register.js');
 
