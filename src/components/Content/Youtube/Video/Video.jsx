f0d4108529e15bbeb91622f8078789d63b92308f
Mathew Cullen <matcullen88@gmail.com>
2016-11-22 14:25:09 -0500

    added react youtube

--- src/components/Content/Youtube/Video/Video.jsx
+++ src/components/Content/Youtube/Video/Video.jsx
@@ -1,18 +1,61 @@
+// adapted from https://github.com/troybetz/react-youtube
+
 // import the libs we need
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YouTube from '../';
 import '../../../normalize.css';
 import style from './Video.css';
 
-// create a React Component called Video
-class Video extends Component {
+const videoIdA = 'XxVg_s8xAms';
+const videoIdB = '-DX3vJiqxm4';
 
-  render(){
+class Video extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoId: videoIdA,
+      player: null,
+    };
+
+    this.onReady = this.onReady.bind(this);
+    this.onChangeVideo = this.onChangeVideo.bind(this);
+    this.onPlayVideo = this.onPlayVideo.bind(this);
+    this.onPauseVideo = this.onPauseVideo.bind(this);
+  }
+
+  onReady(event) {
+    console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`); // eslint-disable-line
+    this.setState({
+      player: event.target,
+    });
+  }
+
+  onPlayVideo() {
+    this.state.player.playVideo();
+  }
+
+  onPauseVideo() {
+    this.state.player.pauseVideo();
+  }
+
+  onChangeVideo() {
+    this.setState({
+      videoId: this.state.videoId === videoIdA ? videoIdB : videoIdA,
+    });
+  }
+
+  render() {
     return (
-      <div id="video-container">
-        <h1>Video will go here</h1>
+      <div>
+        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
+        <button onClick={this.onPlayVideo}>Play</button>
+        <button onClick={this.onPauseVideo}>Pause</button>
+        <button onClick={this.onChangeVideo}>Change Video</button>
       </div>
     );
   }
 }
 
-export default Video;
+ReactDOM.render(<Video />, document.getElementById('root'));
