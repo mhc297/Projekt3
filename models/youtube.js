122ea847ef42bd2b5de81ab9a0f3c2fb5e7a4488
Darryl Zeigler <Dzeig.j@gmail.com>
2016-11-27 22:19:00 -0500

    yo

--- models/youtube.js
+++ models/youtube.js
@@ -1,18 +1,27 @@
 const db = require('../db/db.js');
 
+function getAllVids(req, res, next) {
+  db.any('SELECT * from likes;')
+    .then((likes) => {
+      res.likes = likes;
+      next();
+    })
+    .catch(error => next(error));
+}
+
 function likeVideo(req, res, next) {
- // Implement adopting a puppy
-   db.none('INSERT INTO  likes(name, url) VALUES($1,$2);',[req.body.name, req.body.url])
+   db.none('INSERT INTO  likes(name) VALUES($1);',[req.body.videoId])
    .then((like) => {
      res.likes = likes;
      next();
    })
    .catch(error => next(error));
 }
+
 function removeLikedVideo(req, res, next) {
  db.none(`DELETE FROM likes WHERE id = $1;`, [req.params.id])
    .then(next())
    .catch(err => next(err));
 }
 
-module.exports = { likeVideo, removeLikedVideo };
+module.exports = { getAllVids, likeVideo, removeLikedVideo };
