45c292571aa5e237e699f85553845baa0f15ef77
Darryl Zeigler <Dzeig.j@gmail.com>
2016-11-28 11:06:14 -0500

    updated content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -9,7 +9,6 @@ import style from './Content.css';
 // create a React Component called _App_
 class Content extends Component {
   render(){
-  console.log(this.props.eventData)
     return (
       <div id="content-container">
         <EventList
