cb0fae889e33475da116fb7674bfc06a46f2cf0a
Darryl Zeigler <Dzeig.j@gmail.com>
2016-11-28 11:04:43 -0500

    updated youtube services

--- services/videos.js
+++ services/videos.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 
 function searchVideos(req, res, next){
-  console.log("Hit and req.params.video is ", req.params.video);
   const searchTerm = req.params.video;
   let key = 'AIzaSyD19PVCE5Ur3U6xrOqyEIH2-X17apGi4C8';
   fetch(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&kind=video&order=viewCount&limit=10&key=${key}`)
