474aed36322d40359728b3563fc37b5dcbae72b8
Mathew Cullen <matcullen88@gmail.com>
2016-11-28 19:52:02 -0500

    reverting

--- db/db.js
+++ db/db.js
@@ -4,6 +4,7 @@ const pgConfig = {  host:     process.env.DB_HOST,
                     port:     process.env.DB_PORT,
                     database: process.env.DB_NAME,
                     user:     process.env.DB_USER,
+                    secret:   process.env.JWT_SECRET,
                     password: process.env.DB_PASS };
 
 const db       = pg(pgConfig);
--- scripts/deployment_cleanup.sh
+++ scripts/deployment_cleanup.sh
@@ -3,7 +3,11 @@
 # @desc Deployment should be as slim as possible, and with any identifying remarks or traces removed (i.e., we don't need to tell anyone how we built our app when it's deployed ;).
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 rm -rf db src scripts webpack.config.js .babelrc .gitignore README.md
+=======
+rm -rfs db src scripts starter_resources lessons webpack.config.js .babelrc .gitignore README.md
+>>>>>>> parent of cbacfb3... sort of functioning
 =======
 rm -rfs db src scripts starter_resources lessons webpack.config.js .babelrc .gitignore README.md
 >>>>>>> parent of cbacfb3... sort of functioning
